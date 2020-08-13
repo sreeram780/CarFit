7680c616f86e48313a31886d00c080bef11918c3
SreenivasulaReddy <sreeram780@users.noreply.github.com>
2020-08-13 21:18:29 +0530

    removed space

--- CarFit/AppDelegate.swift
+++ CarFit/AppDelegate.swift
@@ -10,9 +10,6 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let path = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory,
